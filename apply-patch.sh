ba3235369bf13cc4f224f169087c46a1592278e3
rsamban@gmail.com <ramesh.sambandan@sonicdrivein.com>
2017-12-15 21:10:42 -0700

    changes to task.sh

--- apply-patch.sh
+++ apply-patch.sh
@@ -2,7 +2,8 @@
 
 sed -e "s/{{/'{{/g" -e "s/}}/}}'/g" ../../pivotal/git/pcf-pipelines/install-pcf/azure/pipeline.yml > pipeline-temp-1.yml
 bosh-cli interpolate pipeline-temp-1.yml -o patches/creds-patch.yml > pipeline-temp-2.yml
-bosh-cli interpolate pipeline-temp-2.yml -o patches/create-infrastructure-patch.yml > pipeline-temp-1.yml
+cat pipeline-temp-2.yml | \
+yaml-patch -o patches/create-infrastructure-patch.yml > pipeline-temp-1.yml
 sed -e "s/'{{/{{/g" -e "s/}}'/}}/g" pipeline-temp-1.yml > pipeline-temp-final.yml
 fly format-pipeline -c pipeline-temp-final.yml > pipeline-final.yml
 
